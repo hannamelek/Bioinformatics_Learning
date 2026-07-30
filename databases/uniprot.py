0b62ab7302a7528c632628172306d9c514f98010
Hanna Melek Henry <hannamelek826@gmail.com>
2026-07-30 17:21:33 +0000

    uniprot

--- databases/uniprot.py
+++ databases/uniprot.py
@@ -1,7 +1,22 @@
 import requests
+from Bio import SeqIO
 
 url = "https://rest.uniprot.org/uniprotkb/P04637.fasta"
 
 response = requests.get(url)
 
-print(response.text)
+if response.status_code == 200:
+
+    with open("tp53_uniprot.fasta", "w") as file:
+        file.write(response.text)
+
+    print("Downloaded successfully!")
+
+    record = SeqIO.read("tp53_uniprot.fasta", "fasta")
+
+    print("ID:", record.id)
+    print("Description:", record.description)
+    print("Length:", len(record.seq))
+
+else:
+    print("Download failed.")
