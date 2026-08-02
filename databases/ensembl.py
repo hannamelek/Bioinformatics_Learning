bb62d1102093125c5fced4fca7b27295af225209
Hanna Melek Henry <hannamelek826@gmail.com>
2026-08-02 18:04:26 +0000

    Accessing Individual Fields

--- databases/ensembl.py
+++ databases/ensembl.py
@@ -5,7 +5,25 @@ url = "https://rest.ensembl.org/lookup/symbol/homo_sapiens/BRCA1"
 
 response = requests.get(
     url,
-    headers={"Content-Type": "application/json"}
+    headers={"Content-Type":"application/json"}
 )
 
-print(json.dumps(response.json(), indent=2))
+if response.status_code == 200:
+
+    gene = response.json()
+
+    print(json.dumps(gene, indent=2))
+
+    print("\nGene Information")
+    print("----------------")
+    print("Gene:", gene["display_name"])
+    print("Gene ID:", gene["id"])
+    print("Species:", gene["species"])
+    print("Chromosome:", gene["seq_region_name"])
+    print("Start:", gene["start"])
+    print("End:", gene["end"])
+    print("Biotype:", gene["biotype"])
+
+else:
+
+    print("Request failed.")
